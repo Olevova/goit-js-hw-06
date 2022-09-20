03217b323786d0fbd8547a8e7d9da501c08a08a5
Olevova <olevova1983@gmail.com>
2022-09-20 18:20:30 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".widget")
+const spanEl = document.querySelector(".color")
+const bodyPart = document.body
+console.log(bodyPart);
+
+const onBodyChangeColor = (event) => {
+  const newColor = getRandomHexColor();
+  spanEl.textContent = newColor;
+  bodyPart.style.backgroundColor  = newColor;
+
+}
+
+buttonEl.addEventListener("click", onBodyChangeColor);
